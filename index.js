c154dc751a8d3004a21350f72782f8d156d60b63
Nick Landas <nick.landas@gmail.com>
2022-11-02 11:25:10 -0500

    commit tree a063fed7a761a9d3fc35fb1fe75df65a4b462530

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = recordsArray => {
+    const r = recordsArray.find(recordObj => recordObj.result === 'W');
+    if (r) {
+        return r.year
+    } else {
+        return undefined;
+    }
+}
